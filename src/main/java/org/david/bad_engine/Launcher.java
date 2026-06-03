c4f0ecf18a5f298bb371e90ed37d8d51ef0a3df6
MrGodDavid <davidbodongliu@gmail.com>
2026-06-03 19:52:21 -0400

    Initial commit

---
+++ src/main/java/org/david/bad_engine/Launcher.java
@@ -0,0 +1,14 @@
+package org.david.bad_engine;
+
+/**
+ * Entry class of execution of this engine.
+ *
+ * @author Mr. GodDavid
+ * @since 6/3/2026
+ */
+public final class Launcher {
+
+    public static void main(String[] args) {
+
+    }
+}
